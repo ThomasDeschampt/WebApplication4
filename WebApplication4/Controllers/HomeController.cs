84d5f109bef8cf440db3b9e418621938e9c513d7
Thomas Deschampt <thomas.deschampt@epsi.fr>
2023-06-27 20:59:02 +0200

    ui

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontGSB.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "La page de description de votre application";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Vos contacts";
+
+            return View();
+        }
+    }
+}
